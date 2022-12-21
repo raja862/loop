7de045f7d221eaffb104b2112921266234acf22d
N Rajakumaran <108915966+raja862@users.noreply.github.com>
2022-12-21 23:52:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+
+const obj=[{
+"name":"raja",
+"age":"26",
+"designation ":"full stack devl",
+"address":"chennai",
+"pincode":"600020"
+
+}
+]
+//for in
+for(let i = 0; i < obj.length; i++) {
+    
+
+    console.log(obj[i])
+}
+   //for of
+
+for(let rak of obj ){
+    console.log(rak.name)
+}
+
